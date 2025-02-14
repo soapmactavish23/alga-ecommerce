65a7b87b178e3ff38a09e1242eb6c4c1da89aaff
Henrick <ricknogueira1231@gmail.com>
2025-02-14 16:30:32 -0300

    2.4. Criando o EntityManager

---
+++ src/main/java/com/algaworks/util/IniciarUnidadeDePersistencia.java
@@ -0,0 +1,24 @@
+package com.algaworks.util;
+
+import com.algaworks.model.Produto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class IniciarUnidadeDePersistencia {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(produto.getNome());
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+    }
+
+}
--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,2 +1,1 @@
-INSERT INTO Produto (id, nome, preco, descricao) VALUES
-(1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora na pauma da sua mão')
+insert into Produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
