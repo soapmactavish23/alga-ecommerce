7a871a6ffa7ca6efb6721494d2f51ca4cea623d6
Henrick <ricknogueira1231@gmail.com>
2025-02-20 13:47:11 -0300

    3.9. Configurando a geração de identificador com @TableGenerator

--- src/main/java/com/algaworks/model/Categoria.java
+++ src/main/java/com/algaworks/model/Categoria.java
@@ -9,8 +9,15 @@ import lombok.Data;
 public class Categoria {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq", sequenceName = "sequencias_chave_primaria")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+    @TableGenerator(name = "tabela",
+            table = "hibernate_sequences",
+            pkColumnName = "sequence_name",
+            pkColumnValue = "categoria",
+            valueColumnName = "next_val",
+            initialValue = 0,
+            allocationSize = 50
+    )
     private Integer id;
 
     private String nome;
