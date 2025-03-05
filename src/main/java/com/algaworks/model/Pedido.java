d6b41a2d3f4a5433f38aef01242304c23fd89c80
Henrick <ricknogueira1231@gmail.com>
2025-03-05 16:40:02 -0300

    6.2. Exercício: anotação @Column

--- src/main/java/com/algaworks/model/Pedido.java
+++ src/main/java/com/algaworks/model/Pedido.java
@@ -23,10 +23,10 @@ public class Pedido {
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
-    @Column(name = "data_cricacao")
+    @Column(name = "data_cricacao", updatable = false)
     private LocalDateTime dataCriacao;
 
-    @Column(name = "data_ultima_atualizacao")
+    @Column(name = "data_ultima_atualizacao", insertable = false)
     private LocalDateTime dataUltimaAtualizacao;
 
     @Column(name = "data_conclusao")
