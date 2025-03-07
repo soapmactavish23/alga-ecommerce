c9b8d7a3050ab6eacf78dc26b1d05a93108d0258
Henrick <ricknogueira1231@gmail.com>
2025-03-07 11:56:15 -0300

    6.14. Mapeando tabela secundária com @SecondaryTable

--- src/main/java/com/algaworks/model/Cliente.java
+++ src/main/java/com/algaworks/model/Cliente.java
@@ -3,12 +3,14 @@ package com.algaworks.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 @Data
 @Entity
 @Table(name = "cliente")
+@SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"))
 public class Cliente {
 
     @Id
@@ -28,7 +30,11 @@ public class Cliente {
     private String primeiroNome;
 
     @Enumerated(EnumType.STRING)
+    @Column(table = "cliente_detalhe")
     private SexoCliente sexo;
+
+    @Column(name = "data_nascimento", table = "cliente_detalhe")
+    private LocalDate dataNascimento;
 
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos;
---
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/SecondaryTableTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.ecommerce.mapeamentoavancado;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.Cliente;
+import com.algaworks.model.SexoCliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class SecondaryTableTest extends EntityManagerTest {
+
+    @Test
+    public void salvarCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Carlos Finotti");
+        cliente.setSexo(SexoCliente.MASCULINO);
+        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(clienteVerificacao.getSexo());
+    }
+}
