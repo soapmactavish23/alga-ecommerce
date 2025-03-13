e9b32be01fb777dca3ec5dc5d8726799bbc21b39
Henrick <ricknogueira1231@gmail.com>
2025-03-13 18:57:45 -0300

    7.10. Entendendo alguns detalhes de @JoinTable

--- src/main/java/com/algaworks/model/Produto.java
+++ src/main/java/com/algaworks/model/Produto.java
@@ -38,8 +38,10 @@ public class Produto extends EntidadeBaseInteger {
 
     @ManyToMany
     @JoinTable(name = "produto_categoria",
-            joinColumns = @JoinColumn(name = "produto_id"),
-            inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+            joinColumns = @JoinColumn(name = "produto_id", nullable = false,
+                    foreignKey = @ForeignKey(name = "fk_produto_categoria_produto")),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id", nullable = false,
+                    foreignKey = @ForeignKey(name = "fk_produto_categoria_categoria")))
     private List<Categoria> categorias;
 
     @OneToOne(mappedBy = "produto")
