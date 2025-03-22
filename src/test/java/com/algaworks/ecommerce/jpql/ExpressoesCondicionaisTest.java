7fa8a1c2e5ebae873f7f4c91c4c59969cf05eea9
Henrick <ricknogueira1231@gmail.com>
2025-03-22 14:49:51 -0300

    9.13. Usando expressões condicionais is null e is empty

--- src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -10,6 +10,26 @@ import java.util.List;
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
 
     @Test
+    public void usarIsNull() {
+        String jpql = "select p from Produto p where p.foto is null";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsEmpty() {
+        String jpql = "select p from Produto p where p.categorias is empty";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void usarExpressaoCondicionalLike() {
         String jpql = "select c from Cliente c where c.nome like concat('%', :nome, '%')";
 
