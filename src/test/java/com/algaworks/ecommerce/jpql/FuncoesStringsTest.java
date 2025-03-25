68d2195f0bde5fd8e957ce892dbac5a0b3f58fc3
Henrick <ricknogueira1231@gmail.com>
2025-03-25 14:09:48 -0300

    9.23. Usando funções para datas

--- src/test/java/com/algaworks/ecommerce/jpql/FuncoesStringsTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/FuncoesStringsTest.java
@@ -10,7 +10,20 @@ import java.util.List;
 public class FuncoesStringsTest extends EntityManagerTest {
 
     @Test
-    public void aplicarFuncao() {
+    public void aplicarFuncaoData() {
+        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) " +
+                " from Pedido p where hour(p.dataCriacao) > 18";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoString() {
         String jpql = "select c.nome, length(c.nome) from Categoria c " +
                 "where substring(c.nome, 1, 1) = 'N'";
 
