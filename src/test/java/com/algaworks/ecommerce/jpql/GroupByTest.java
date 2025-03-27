71d66bfc9f3f8133a51a4243500ef07357ba6cb5
Henrick <ricknogueira1231@gmail.com>
2025-03-27 14:45:46 -0300

    9.31. Usando o having para condicionar o agrupamento

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -10,6 +10,21 @@ import java.util.List;
 public class GroupByTest extends EntityManagerTest {
 
     @Test
+    public void condicionarAgrupamentoComHaving() {
+        String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
+                " join ip.produto pro join pro.categorias cat " +
+                " group by cat.id " +
+                " having sum(ip.precoProduto) > 100";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+
+    }
+
+    @Test
     public void agruparEFiltrarResultado() {
 //         Total de vendas por mês.
 //        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
@@ -18,16 +33,16 @@ public class GroupByTest extends EntityManagerTest {
 //                " group by year(p.dataCriacao), month(p.dataCriacao) ";
 
 //         Total de vendas por categoria.
-//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
-//                " join ip.produto pro join pro.categorias c join ip.pedido p " +
-//                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
-//                " group by c.id";
+        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+                " join ip.produto pro join pro.categorias c join ip.pedido p " +
+                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
+                " group by c.id";
 
 //        Total de vendas por cliente
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
-                " join ip.pedido p join p.cliente c join ip.pedido p " +
-                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) " +
-                " group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c join ip.pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) " +
+//                " group by c.id";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
