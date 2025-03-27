7aba20c63d0baf98caeda1a889f7570bdf7c7b76
Henrick <ricknogueira1231@gmail.com>
2025-03-27 14:39:03 -0300

    9.30. Usando a cláusula where com group by

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -10,6 +10,35 @@ import java.util.List;
 public class GroupByTest extends EntityManagerTest {
 
     @Test
+    public void agruparEFiltrarResultado() {
+//         Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+
+//         Total de vendas por categoria.
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c join ip.pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+                " join ip.pedido p join p.cliente c join ip.pedido p " +
+                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) " +
+                " group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
     public void agruparResultado() {
 //         Quantidade de produtos por categoria.
 //        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
