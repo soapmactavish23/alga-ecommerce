02bbe6f79c9d2381cc51bb43b9a55dd6b626f002
Henrick <ricknogueira1231@gmail.com>
2025-03-29 14:18:55 -0300

    9.35. Criando subqueries

---
+++ src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -0,0 +1,39 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.Cliente;
+import jakarta.persistence.TypedQuery;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class SubqueriesTest extends EntityManagerTest {
+
+    @Test
+    public void pesquisarSubqueries() {
+//         Bons clientes. Versão 2.
+        String jpql = "select c from Cliente c where " +
+                " 500 < (select sum(p.total) from Pedido p where p.cliente = c)";
+
+//         Bons clientes. Versão 1.
+//        String jpql = "select c from Cliente c where " +
+//                " 500 < (select sum(p.total) from c.pedidos p)";
+
+//         Todos os pedidos acima da média de vendas
+//        String jpql = "select p from Pedido p where " +
+//                " p.total > (select avg(total) from Pedido)";
+
+//         O produto ou os produtos mais caros da base.
+//        String jpql = "select p from Produto p where " +
+//                " p.preco = (select max(preco) from Produto)";
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+
+        List<Cliente> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
+    }
+
+}
