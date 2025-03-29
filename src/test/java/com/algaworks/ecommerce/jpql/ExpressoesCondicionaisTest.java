7d5a509370a333ae3da0345cb045d66d9892f243
Henrick <ricknogueira1231@gmail.com>
2025-03-29 14:07:47 -0300

    9.33. Usando a expressão in

--- src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.Cliente;
 import com.algaworks.model.Pedido;
 import com.algaworks.model.Produto;
 import jakarta.persistence.TypedQuery;
@@ -9,9 +10,29 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoIN() {
+        Cliente cliente1 = new Cliente();
+        cliente1.setId(1);
+
+        Cliente cliente2 = new Cliente();
+        cliente2.setId(2);
+
+        List<Cliente> clientes = Arrays.asList(cliente1, cliente2);
+
+        String jpql = "select p from Pedido p where p.cliente in (:clientes)";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        typedQuery.setParameter("clientes", clientes);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void usarExpressaoCase() {
