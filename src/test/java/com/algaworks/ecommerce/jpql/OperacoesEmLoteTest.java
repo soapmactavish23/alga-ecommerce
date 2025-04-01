0ef5a56a20cdb3fc1a63e46359a2c63c120c1160
Henrick <ricknogueira1231@gmail.com>
2025-04-01 12:23:12 -0300

    9.44. Fazendo operações em lote

---
+++ src/test/java/com/algaworks/ecommerce/jpql/OperacoesEmLoteTest.java
@@ -0,0 +1,56 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.Produto;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+public class OperacoesEmLoteTest extends EntityManagerTest {
+
+    private static final int LIMITE_INSERCOES = 4;
+
+    @Test
+    public void inserirEmLote() throws IOException {
+        InputStream in = OperacoesEmLoteTest.class.getClassLoader()
+                .getResourceAsStream("produtos/importar.txt");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        entityManager.getTransaction().begin();
+
+        int contadorInsercoes = 0;
+
+        for(String linha: reader.lines().collect(Collectors.toList())) {
+            if (linha.isBlank()) {
+                continue;
+            }
+
+            String[] produtoColuna = linha.split(";");
+            Produto produto = new Produto();
+            produto.setNome(produtoColuna[0]);
+            produto.setDescricao(produtoColuna[1]);
+            produto.setPreco(new BigDecimal(produtoColuna[2]));
+            produto.setDataCriacao(LocalDateTime.now());
+
+            entityManager.persist(produto);
+
+            if (++contadorInsercoes == LIMITE_INSERCOES) {
+                entityManager.flush();
+                entityManager.clear();
+
+                contadorInsercoes = 0;
+
+                System.out.println("---------------------------------");
+            }
+        }
+
+        entityManager.getTransaction().commit();
+    }
+}
