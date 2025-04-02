1782c8b74ff9932340fe5246169aa2a35c6963b5
Henrick <ricknogueira1231@gmail.com>
2025-04-02 14:28:29 -0300

    9.45. Atualizando objetos em lote

--- src/test/java/com/algaworks/ecommerce/jpql/OperacoesEmLoteTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/OperacoesEmLoteTest.java
@@ -2,6 +2,7 @@ package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
 import com.algaworks.model.Produto;
+import jakarta.persistence.Query;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -15,6 +16,20 @@ import java.util.stream.Collectors;
 public class OperacoesEmLoteTest extends EntityManagerTest {
 
     private static final int LIMITE_INSERCOES = 4;
+
+    @Test
+    public void atualizarEmLote() {
+        entityManager.getTransaction().begin();
+
+        String jpql = "update Produto p set p.preco = p.preco + (p.preco * 0.1) " +
+                " where exists (select 1 from p.categorias c2 where c2.id = :categoria)";
+
+        Query query = entityManager.createQuery(jpql);
+        query.setParameter("categoria", 2);
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
 
     @Test
     public void inserirEmLote() throws IOException {
