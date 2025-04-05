098369019556db553abeae89202f4d5f905e64a0
Henrick <ricknogueira1231@gmail.com>
2025-04-05 13:56:06 -0300

    10.4. Trabalhando com projeções

--- src/test/java/com/algaworks/ecommerce/criterion/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criterion/BasicoCriteriaTest.java
@@ -16,6 +16,22 @@ import java.util.List;
 public class BasicoCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void projetarOResultado() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.multiselect(root.get("id"), root.get("nome"));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println("ID: " + arr[0] + ", Nome: " + arr[1]));
+
+    }
+
+    @Test
     public void retornarTodosOsProdutosExercicio() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
