06c47d2acd3bc97e6a43bdf573e25fd183d995e8
Henrick <ricknogueira1231@gmail.com>
2025-04-07 14:03:05 -0300

    10.10. Fazendo o join e usando o fetch

--- src/test/java/com/algaworks/ecommerce/criterion/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criterion/JoinCriteriaTest.java
@@ -9,9 +9,30 @@ import jakarta.persistence.criteria.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usarJoinFetch() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        root.fetch("notaFiscal", JoinType.LEFT);
+        root.fetch("pagamento", JoinType.LEFT);
+        root.fetch("cliente");
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido);
+    }
 
     @Test
     public void fazerLeftOuterJoin() {
