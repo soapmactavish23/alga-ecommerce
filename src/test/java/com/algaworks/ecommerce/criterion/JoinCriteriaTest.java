70bafb636cb821ef358402fc0aa46d933a227449
Henrick <ricknogueira1231@gmail.com>
2025-04-07 13:50:08 -0300

    10.8. Usando a cláusula on no join

--- src/test/java/com/algaworks/ecommerce/criterion/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criterion/JoinCriteriaTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.criterion;
 import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
 import com.algaworks.model.Pagamento;
 import com.algaworks.model.Pedido;
+import com.algaworks.model.StatusPagamento;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -14,6 +15,22 @@ import org.junit.Test;
 import java.util.List;
 
 public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoinComOn() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        joinPagamento.on(criteriaBuilder.equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> pedidos = typedQuery.getResultList();
+        Assert.assertTrue(pedidos.isEmpty());
+
+    }
 
     @Test
     public void fazerJoin() {
