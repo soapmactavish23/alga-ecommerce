12308d95d384878be532e84797c9ced71062dd8c
Henrick <ricknogueira1231@gmail.com>
2025-04-08 15:07:18 -0300

    10.14. Usando expressão condicional like

---
+++ src/test/java/com/algaworks/ecommerce/criterion/ExpressoesCondicionaisCriteriaTest.java
@@ -0,0 +1,31 @@
+package com.algaworks.ecommerce.criterion;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.Cliente;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.like(criteriaBuilder.lower(root.get("nome")),"%a%"));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Cliente> clientes = typedQuery.getResultList();
+        Assert.assertFalse(clientes.isEmpty());
+    }
+
+}
