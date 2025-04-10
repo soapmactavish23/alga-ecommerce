ba9582b96246d4761fb3af7da1e952fb2c061fa9
Henrick <ricknogueira1231@gmail.com>
2025-04-10 20:56:57 -0300

    10.24. Usando funções para datas

--- src/main/java/com/algaworks/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/model/PagamentoBoleto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @DiscriminatorValue("boleto")
@@ -14,4 +16,8 @@ public class PagamentoBoleto extends Pagamento {
 
     @Column(name = "codigo_barras", length = 100)
     private String codigoBarras;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
 }
---
+++ src/test/java/com/algaworks/ecommerce/criterion/FuncoesCriteriaTest.java
@@ -0,0 +1,89 @@
+package com.algaworks.ecommerce.criterion;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.*;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FuncoesCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoData() {
+        // current_date, current_time, current_timestamp
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        Join<Pedido, PagamentoBoleto> joinPagamentoBoleto = criteriaBuilder
+                .treat(joinPagamento, PagamentoBoleto.class);
+
+        criteriaQuery.multiselect(
+                root.get("id"),
+                criteriaBuilder.currentDate(),
+                criteriaBuilder.currentTime(),
+                criteriaBuilder.currentTimestamp()
+        );
+
+        criteriaQuery.where(
+                criteriaBuilder.between(criteriaBuilder.currentDate(),
+                        root.get("dataCriacao").as(java.sql.Date.class),
+                        joinPagamentoBoleto.get("dataVencimento").as(java.sql.Date.class)),
+                criteriaBuilder.equal(root.get("status"), StatusPedido.AGUARDANDO)
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                        + ", current_date: " + arr[1]
+                        + ", current_time: " + arr[2]
+                        + ", current_timestamp: " + arr[3]));
+    }
+
+    @Test
+    public void aplicarFuncoesString() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        Path fieldName = root.get("nome");
+
+        criteriaQuery.multiselect(
+                root.get("nome"),
+                criteriaBuilder.concat("Nome do cliente: ", fieldName),
+                criteriaBuilder.length(fieldName),
+                criteriaBuilder.locate(fieldName, "a"),
+                criteriaBuilder.substring(fieldName, 1, 2),
+                criteriaBuilder.lower(fieldName),
+                criteriaBuilder.upper(fieldName),
+                criteriaBuilder.trim(fieldName)
+        );
+
+        criteriaQuery.where(criteriaBuilder.equal(criteriaBuilder.substring(fieldName, 1, 1), "M"));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                    + ", concat: " + arr[1]
+                    + ", length: " + arr[2]
+                    + ", locale: " + arr[3]
+                    + ", substring: " + arr[4]
+                    + ", lower: " + arr[5]
+                    + ", upper: " + arr[6]
+                    + ", trim: " + arr[7]
+        ));
+
+    }
+
+}
