57a97606aed8bfa11a30ac0a7ec9a54812e5d7de
Henrick <ricknogueira1231@gmail.com>
2025-04-12 15:02:52 -0300

    10.30. Exercício: usando group by

--- src/test/java/com/algaworks/ecommerce/criterion/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criterion/GroupByCriteriaTest.java
@@ -1,9 +1,7 @@
 package com.algaworks.ecommerce.criterion;
 
 import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
-import com.algaworks.model.Categoria;
-import com.algaworks.model.ItemPedido;
-import com.algaworks.model.Produto;
+import com.algaworks.model.*;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -34,6 +32,32 @@ public class GroupByCriteriaTest extends EntityManagerTest {
         List<Object[]> lista = typedQuery.getResultList();
         lista.forEach(arr -> System.out.println("Nome categoria: " + arr[0] + ", sum: " + arr[1]));
 
+    }
+
+    @Test
+    public void agruparResultado03Exercicio() {
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+        Join<ItemPedido, Pedido> joinPedido = root.join("pedido");
+        Join<Pedido, Cliente> joinPedidoCliente = joinPedido.join("cliente");
+
+        criteriaQuery.multiselect(
+                joinPedidoCliente.get("nome"),
+                criteriaBuilder.sum(root.get("precoProduto"))
+        );
+
+        criteriaQuery.groupBy(joinPedidoCliente.get("id"));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr -> System.out.println("Nome cliente: " + arr[0] + ", Sum: " + arr[1]));
     }
 
 }
