b1c9485d9b3bca7d1914b982737d9885f747ecc9
Henrick <ricknogueira1231@gmail.com>
2025-04-14 14:09:06 -0300

    10.32. Exercício: consultando pedidos com produto específico

--- src/test/java/com/algaworks/ecommerce/criterion/PathExpressionsTest.java
+++ src/test/java/com/algaworks/ecommerce/criterion/PathExpressionsTest.java
@@ -9,9 +9,30 @@ import jakarta.persistence.criteria.Root;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class PathExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoDiferente() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.notEqual(
+                root.get("total"), new BigDecimal(499)));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(p -> System.out.println(
+                "ID: " + p.getId() + ", Total: " + p.getTotal()));
+    }
+
 
     @Test
     public void usarPathExpression() {
