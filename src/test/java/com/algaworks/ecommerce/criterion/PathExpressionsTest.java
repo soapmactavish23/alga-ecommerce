a7da31eb6ac3f7e9c91db50dcffa6b40e5d60803
Henrick <ricknogueira1231@gmail.com>
2025-04-14 14:07:06 -0300

    10.31. Diferença entre expressions, paths e predicates

---
+++ src/test/java/com/algaworks/ecommerce/criterion/PathExpressionsTest.java
@@ -0,0 +1,31 @@
+package com.algaworks.ecommerce.criterion;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.model.Pedido;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class PathExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void usarPathExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.like(root.get("cliente").get("nome"), "M%"));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+}
